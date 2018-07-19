64df6f2832735c092f71dec26c5fb980da90c0a3
543701603@qq.com <wu15979937502>
2018-07-19 13:35:08 +0800

    提交一把

---
+++ app/src/main/java/myyl/com/myyl/model/MyZxInfo.java
@@ -0,0 +1,42 @@
+package myyl.com.myyl.model;
+
+import java.io.Serializable;
+
+public class MyZxInfo implements Serializable {
+    private String userUrl;
+    private String userName;
+    private String time;
+    private String content;
+
+    public String getUserUrl() {
+        return userUrl;
+    }
+
+    public void setUserUrl(String userUrl) {
+        this.userUrl = userUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
